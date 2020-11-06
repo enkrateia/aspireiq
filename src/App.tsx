31f74c3dd11b95116dadf0fc45a3b2f09d3aed06
Carlos Govea <cgovea0310@gmail.com>
2020-11-05 19:13:45 -0600

    Replacing app default code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import EmailInput from 'components/EmailInput';
 
-function App() {
+interface AppProps {}
+
+const App: React.FunctionComponent<AppProps> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.appContainer}>
+      <EmailInput />
     </div>
   );
-}
+};
 
 export default App;
